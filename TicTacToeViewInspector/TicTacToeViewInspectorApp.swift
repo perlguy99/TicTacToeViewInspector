7d1d1a464a6a8759dfed5e4513d731d8b05df26e
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-24 08:36:56 -0500

    Initial Commit

---
+++ TicTacToeViewInspector/TicTacToeViewInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeViewInspectorApp.swift
+//  TicTacToeViewInspector
+//
+//  Created by Brent Michalski on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeViewInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
