7f9ce79f166da7c43af849d63e85c66d7015dac7
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-27 14:50:32 -0500

    . t Adding tests for all squares on a game board

--- TicTacToeViewInspectorTests/GameBoardViewTests.swift
+++ TicTacToeViewInspectorTests/GameBoardViewTests.swift
@@ -183,4 +183,54 @@ final class GameBoardViewTests: XCTestCase {
         sut.controller.isDraw = true
         XCTAssertEqual(try playAgainButtonOpacity(), 1.0)
     }
+    
+    // TODO: - Need to write tests for actual squares
+    func testSquareViewsDisplayCorrectState_Foo() throws {
+        // Given that I have a Game Board
+        let sut = GameBoardView()
+
+        // And I take a square from the game board
+        let foo = sut.controller.gameBoard[0]
+        
+        // I can create a Mock SquareView, and test against it!
+        let bar = SquareView(square: foo)
+        
+        let displayedImage = try bar.inspect().find(viewWithTag: "SquareView.Rectangle").overlay().image().actualImage()
+        XCTAssertEqual(try displayedImage.name(), try! stateEmptyImage.name())
+    }
+    
+    func testAllSquaresPart1() throws {
+        let sut = GameBoardView()
+        sut.controller.takeTurnAt(0)
+        sut.controller.takeTurnAt(1)
+        sut.controller.takeTurnAt(2)
+        
+        try verifyAllNineSquaresForCorrectState(gameBoardView: sut)
+    }
+    
+    // Taking what we have above, let's make this much easier!
+    func getDisplayedImageForSquare(_ square: Square) throws -> Image {
+        let squareView = SquareView(square: square)
+        let displayedImage = try squareView.inspect().find(viewWithTag: "SquareView.Rectangle").overlay().image().actualImage()
+        return displayedImage
+    }
+    
+    // And now test ALL 9 squares at once
+    func verifyAllNineSquaresForCorrectState(gameBoardView: GameBoardView) throws {
+        // Using the GameBoard passed in...
+        
+        // All 9 should have no image currently
+        // Expecting this to fail
+        for index in 0...8 {
+            let square = gameBoardView.controller.gameBoard[index]
+            let displayedImage = try getDisplayedImageForSquare(square)
+            let displayedImageName = try displayedImage.name()
+            let squareStateImageName = square.squareImageName
+            
+            XCTAssertEqual(displayedImageName, squareStateImageName, "At index \(index): \(displayedImageName) does not match: \(squareStateImageName)")
+        }
+        
+    }
+    
 }
+
